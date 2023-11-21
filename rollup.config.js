73b7916b9b48e3572191496f7abeae2c1ba586a3
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2023-11-21 10:29:49 -0800

    updated rollup file

--- rollup.config.js
+++ rollup.config.js
@@ -2,24 +2,38 @@ import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { dts } from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.tsx',
-  output: [
-    {
-      file: 'dist/cjs/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: 'dist/esm/index.js',
-      format: 'esm',
-      sourcemap: true,
-    },
-    {
-      file: 'dist/types/types.d.ts',
-      format: 'es',
-    },
-  ],
-  plugins: [nodeResolve(), typescript(), dts()],
-  external: ['react', 'react-dom'],
-};
+export default [
+  // Configuration for JavaScript Bundle
+  {
+    // entry point for the bundle file where rollup starts bundling
+    input: 'src/index.tsx',
+
+    // defines how and where the bundled output should be saved
+    output: [
+      // configuration in CommonJS for Node.js enviornments
+      {
+        file: 'dist/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      // configuration in ES Module format for Browsers and ES Module enviornments
+      {
+        file: 'dist/esm/index.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+
+    // plugins used for configuration
+    plugins: [nodeResolve(), typescript()],
+
+    // dependencies not provided by bundle but should be in consumers enviornment
+    external: ['react', 'react-dom'],
+  },
+  // Configuration for TypeScript Declaration Files
+  {
+    input: 'src/types.d.ts',
+    output: [{ file: 'dist/types/types.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
