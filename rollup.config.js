b00ea8b6d84f979b749a515faa3cf68d8dd7ab24
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2023-11-21 09:36:54 -0800

    rollup working

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import dts from 'rollup/plugin-dts';
+import { dts } from 'rollup-plugin-dts';
 
 export default {
   input: 'src/index.tsx',
