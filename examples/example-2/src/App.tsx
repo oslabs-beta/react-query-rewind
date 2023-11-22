6dd45784bb94b5ec8481ba25017e4052e085dbb6
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2023-11-21 18:00:20 -0800

    saving before merge

--- examples/example-2/src/App.tsx
+++ examples/example-2/src/App.tsx
@@ -1,5 +1,17 @@
+import { useState } from 'react';
+
 function App() {
-  return 'Example 2';
+  const [postsArray, setPostsArray] = useState([]);
+
+  return (
+    <>
+      <div className="window">
+        <div className="nav-bar"></div>
+        <div className="create-post-container"></div>
+        <div className="posts-container"></div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- examples/example-2/src/main.tsx
+++ examples/example-2/src/main.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient } from '@tanstack/react-query';
+import './styles.css';
+
+export const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>
+    <ReactQueryDevtools />
+  </QueryClientProvider>
+  // </React.StrictMode>
 );
