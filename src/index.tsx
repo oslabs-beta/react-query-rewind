6ef8ac9db4c40fbe6dabea8fce8b2d1eab1ea76b
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2023-11-25 14:32:59 -0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,13 @@ import { QueryClient, useQueryClient } from '@tanstack/react-query';
 import { SubscribeEvent } from './types';
 // import formatAndSendToChrome from './lib/rewind'
 
-// Test function so we can see data in the console
-const logging = (event: SubscribeEvent) => {
-  // need to parse through this data and send it to the chrome extension
+// DATA TO SEND
+//
 
+const logging = (event: SubscribeEvent) => {
   const simplifiedObj = {
     type: event.type,
-    time: event.query.state.dataUpdatedAt, //might need to format this as a datetime
+    time: event.query.state.dataUpdatedAt,
     queryKey: event.query.queryKey,
     func: event.query.options.queryFn,
     data: event.query.state.data,
@@ -20,34 +20,26 @@ const logging = (event: SubscribeEvent) => {
     action: event.action ? event.action.type : null,
   };
 
-  // console.log(event);
-
   const importantTypes = ['updated', 'removed', 'added'];
   if (importantTypes.includes(simplifiedObj.type)) {
-    console.log(simplifiedObj);
+    console.log(event);
   }
 };
 
 const ReactQueryRewind = () => {
-  // React does not allow hooks inside of useEffect
   const queryClient = useQueryClient();
+
   useEffect(() => {
     const queryCache = queryClient.getQueryCache();
-    // tbh this is a callback - it will only every take in one event. You can't accidentally call this function. It looks like there are ways to do partial checks in enormous objects but it might not be worth it for us. We can type check the fields we care about and send those along to the pure functions
+
     const unsubscribe = queryCache.subscribe((event: SubscribeEvent) => {
-      // setTimeout ensure it runs after components load
-      setTimeout(() => {
-        // These need to be optimized so that if it's data I don't want, the functions are never called or return as early as possible
-        // formatAndSendToChrome(event.query.queryKey, event.query.state.data);
-        // for testing purposes
-        logging(event);
-      }, 0);
+      logging(event);
     });
 
     return () => unsubscribe();
-  }, []); // Because this component is imported at the root of the app, it re-runs every time anything in the app changes
-  // return a fragment to keep react happy, but this affects the build
-  return <></>;
+  }, []);
+
+  return null;
 };
 
 export default ReactQueryRewind;
