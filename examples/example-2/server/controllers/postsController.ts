2833acfd705c683f32a9c33b5f867d528835669f
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2023-11-26 22:17:53 -0800

    implemented routes

---
+++ examples/example-2/server/controllers/postsController.ts
@@ -0,0 +1,167 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { Request, Response, NextFunction } from 'express';
+
+type PostsController = {
+  fetchData: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+  createPost: (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => Promise<void>;
+  likePost: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+  deletePost: (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => Promise<void>;
+  createComment: (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => Promise<void>;
+  openComment: (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => Promise<void>;
+};
+
+const postsController: PostsController = {
+  fetchData: async (req, res, next) => {
+    try {
+      const data = await fs.readFile(
+        path.join(__dirname, '../../models/database.json'),
+        'utf8'
+      );
+      const db = JSON.parse(data);
+
+      res.locals.fetchData = db.posts;
+
+      next();
+    } catch (err) {
+      console.error('Error reading from database.json:', err);
+      res.status(500).send('Error reading data');
+    }
+  },
+
+  createPost: async (req, res, next) => {
+    try {
+      const dbPath = path.join(__dirname, '../../models/database.json');
+      const data = await fs.readFile(dbPath, 'utf8');
+      const db = JSON.parse(data);
+
+      const newPost = req.body;
+      db.posts.unshift(newPost);
+
+      await fs.writeFile(dbPath, JSON.stringify(db, null, 2), 'utf8');
+
+      res.locals.createPost = db.posts;
+
+      next();
+    } catch (err) {
+      console.error('Error updating database.json:', err);
+      res.status(500).send('Error saving data');
+    }
+  },
+
+  likePost: async (req, res, next) => {
+    try {
+      const dbPath = path.join(__dirname, '../../models/database.json');
+      const data = await fs.readFile(dbPath, 'utf8');
+      const db = JSON.parse(data);
+
+      const index = parseInt(req.body.index, 10);
+
+      //   const updatedPostsArray = db.posts.map((post, curIndex) => {
+      //     return {
+      //       ...post,
+      //       liked: index === curIndex ? !post.liked : post.liked,
+      //     };
+      //   });
+
+      //   db.posts = updatedPostsArray;
+
+      db.posts[index].liked = !db.posts[index].liked;
+
+      await fs.writeFile(dbPath, JSON.stringify(db, null, 2), 'utf8');
+
+      res.locals.likePost = db.posts;
+
+      next();
+    } catch (err) {
+      console.error('Error updating database.json:', err);
+      res.status(500).send('Error saving data');
+    }
+  },
+
+  deletePost: async (req, res, next) => {
+    try {
+      const dbPath = path.join(__dirname, '../../models/database.json');
+      const data = await fs.readFile(dbPath, 'utf8');
+      const db = JSON.parse(data);
+
+      const index = parseInt(req.body.index, 10);
+
+      const updatedPostsArray = db.posts.filter((_, curIndex) => {
+        return curIndex !== index;
+      });
+
+      db.posts = updatedPostsArray;
+
+      await fs.writeFile(dbPath, JSON.stringify(db, null, 2), 'utf8');
+
+      res.locals.deletePost = db.posts;
+
+      next();
+    } catch (err) {
+      console.error('Error updating database.json:', err);
+      res.status(500).send('Error saving data');
+    }
+  },
+
+  createComment: async (req, res, next) => {
+    try {
+      const dbPath = path.join(__dirname, '../../models/database.json');
+      const data = await fs.readFile(dbPath, 'utf8');
+      const db = JSON.parse(data);
+
+      const { index, comment } = req.body;
+      const postIndex = parseInt(index, 10);
+
+      db.posts[postIndex].comments.push(comment);
+
+      await fs.writeFile(dbPath, JSON.stringify(db, null, 2), 'utf8');
+
+      res.locals.createComment = db.posts;
+
+      next();
+    } catch (err) {
+      console.error('Error updating database.json:', err);
+      res.status(500).send('Error saving data');
+    }
+  },
+
+  openComment: async (req, res, next) => {
+    try {
+      const dbPath = path.join(__dirname, '../../models/database.json');
+      const data = await fs.readFile(dbPath, 'utf8');
+      const db = JSON.parse(data);
+
+      const index = parseInt(req.body.index, 10);
+
+      db.posts[index].createComment = !db.posts[index].create;
+
+      await fs.writeFile(dbPath, JSON.stringify(db, null, 2), 'utf8');
+
+      res.locals.openComment = db.posts;
+
+      next();
+    } catch (err) {
+      console.error('Error updating database.json:', err);
+      res.status(500).send('Error saving data');
+    }
+  },
+};
+
+export default postsController;
--- examples/example-2/server/server.ts
+++ examples/example-2/server/server.ts
@@ -1,141 +1,14 @@
-const express = require('express');
-const fs = require('fs').promises;
-const path = require('path');
-
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import router from './routes';
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// fetching initital data
-app.get('/fetch-data', async (req, res) => {
-  try {
-    const data = await fs.readFile(
-      path.join(__dirname, 'database.json'),
-      'utf8'
-    );
-    res.status(200).json(JSON.parse(data).posts);
-  } catch (err) {
-    console.error('Error reading from database.json:', err);
-    res.status(500).send('Error reading data');
-  }
-});
-
-// create post
-app.post('/create-post', async (req, res) => {
-  try {
-    const dbPath = path.join(__dirname, 'database.json');
-    const data = await fs.readFile(dbPath, 'utf8');
-    const db = JSON.parse(data);
-
-    const newPost = req.body;
-    db.posts.unshift(newPost);
-
-    await fs.writeFile(dbPath, JSON.stringify(db, null, 2), 'utf8');
-
-    res.status(201).json(db.posts);
-  } catch (err) {
-    console.error('Error updating database.json:', err);
-    res.status(500).send('Error saving data');
-  }
-});
-
-// like post
-app.post('/like-post', async (req, res) => {
-  try {
-    const dbPath = path.join(__dirname, 'database.json');
-    const data = await fs.readFile(dbPath, 'utf8');
-    const db = JSON.parse(data);
-
-    const index = parseInt(req.body.index, 10);
-
-    const updatedPostsArray = db.posts.map((post, curIndex) => {
-      return {
-        ...post,
-        liked: index === curIndex ? !post.liked : post.liked,
-      };
-    });
-
-    db.posts = updatedPostsArray;
-
-    await fs.writeFile(dbPath, JSON.stringify(db, null, 2), 'utf8');
-
-    res.status(201).json(db.posts);
-  } catch (err) {
-    console.error('Error updating database.json:', err);
-    res.status(500).send('Error saving data');
-  }
-});
-
-// delete post
-app.post('/delete-post', async (req, res) => {
-  try {
-    const dbPath = path.join(__dirname, 'database.json');
-    const data = await fs.readFile(dbPath, 'utf8');
-    const db = JSON.parse(data);
-
-    const index = parseInt(req.body.index, 10);
-
-    const updatedPostsArray = db.posts.filter((_, curIndex) => {
-      return curIndex !== index;
-    });
-
-    db.posts = updatedPostsArray;
-
-    await fs.writeFile(dbPath, JSON.stringify(db, null, 2), 'utf8');
-
-    res.status(201).json(db.posts);
-  } catch (err) {
-    console.error('Error updating database.json:', err);
-    res.status(500).send('Error saving data');
-  }
-});
-
-// create commment
-app.post('/create-comment', async (req, res) => {
-  try {
-    const dbPath = path.join(__dirname, 'database.json');
-    const data = await fs.readFile(dbPath, 'utf8');
-    const db = JSON.parse(data);
-
-    const { index, comment } = req.body;
-    const postIndex = parseInt(index, 10);
-
-    db.posts[postIndex].comments.push(comment);
-
-    await fs.writeFile(dbPath, JSON.stringify(db, null, 2), 'utf8');
-
-    res.status(201).json(db.posts);
-  } catch (err) {
-    console.error('Error updating database.json:', err);
-    res.status(500).send('Error saving data');
-  }
-});
-
-// open comment
-app.post('/open-comment', async (req, res) => {
-  try {
-    const dbPath = path.join(__dirname, 'database.json');
-    const data = await fs.readFile(dbPath, 'utf8');
-    const db = JSON.parse(data);
-
-    const index = parseInt(req.body.index, 10);
-
-    db.posts[index].createComment = !db.posts[index].createComment;
-
-    await fs.writeFile(dbPath, JSON.stringify(db, null, 2), 'utf8');
-
-    res.status(201).json(db.posts);
-  } catch (err) {
-    console.error('Error updating database.json:', err);
-    res.status(500).send('Error saving data');
-  }
-});
+app.use(router);
 
 app.use('*', (req, res) => {
   res.status(404).send('Not Found');
@@ -144,3 +17,5 @@ app.use('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+export default express;
