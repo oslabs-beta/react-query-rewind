080639d0a555bff48eabb044a0c74ef944391263
John Dunn <johnwdunn20@gmail.com>
2023-12-01 18:01:36 -0800

    Cleaned up example

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,7 @@ import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-// I think we need to update package.json for this
-import ReactQueryRewind, { RewindHook } from 'react-query-rewind';
-
-import TestReactQueryRewind from './components/TestReactQueryRewind'
+import ReactQueryRewind from 'react-query-rewind';
 
 // create queryClientProvider
 const queryClient = new QueryClient({
@@ -22,7 +19,6 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
-    {/* <TestReactQueryRewind/> */}
     <ReactQueryRewind/>
     <App />
     <ReactQueryDevtools />
