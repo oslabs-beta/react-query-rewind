71f503b4ad1732d6810fbd42850508d42224c166
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2023-12-28 21:50:32 -0800

    fixed duplicate event issue

--- test-extension/background.ts
+++ test-extension/background.ts
@@ -1,27 +1,20 @@
-// This variable will hold the reference to the DevTools page port
+console.log('background.ts loaded');
+
 let devToolsPort: chrome.runtime.Port | null = null;
 
-// Listener for connections from the DevTools page
+chrome.runtime.onMessage.addListener((message: any, sender, sendResponse) => {
+  if (devToolsPort) {
+    console.log(message, 'BACKGROUND.JS');
+    devToolsPort.postMessage(message);
+  }
+});
+
 chrome.runtime.onConnect.addListener((port: chrome.runtime.Port) => {
   if (port.name === 'devtools-panel') {
-    // Store the port for communication with the DevTools panel
     devToolsPort = port;
 
-    // Handle disconnection
     devToolsPort.onDisconnect.addListener(() => {
       devToolsPort = null;
     });
-
-    // Listen for messages from the content script
-    chrome.runtime.onMessage.addListener(
-      (message: any, sender, sendResponse) => {
-        // Check if the DevTools panel is connected
-        if (devToolsPort) {
-          console.log(message, 'background');
-          // Forward the message from the content script to the DevTools panel
-          devToolsPort.postMessage(message);
-        }
-      }
-    );
   }
 });
