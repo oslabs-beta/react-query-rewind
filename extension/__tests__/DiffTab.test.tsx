3be4d1c9f69e7e693d122debaa712b41388684fc
John Dunn <johnwdunn20@gmail.com>
2024-02-22 00:03:52 -0800

    started difftab test

---
+++ extension/__tests__/DiffTab.test.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import DiffTab from '../src/containers/DiffTab';
+import '@testing-library/jest-dom';
+
