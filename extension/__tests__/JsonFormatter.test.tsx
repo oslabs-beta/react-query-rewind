b448ce31bb83157ec547764ea09494c894b6ffef
John Dunn <johnwdunn20@gmail.com>
2024-02-21 23:40:22 -0800

    Added tests and snapshots for jsondiff

--- extension/__tests__/JsonFormatter.test.tsx
+++ extension/__tests__/JsonFormatter.test.tsx
@@ -3,11 +3,13 @@ import { render } from '@testing-library/react';
 import JsonFormatter from '../src/components/JsonFormatter';
 
 describe('JsonFormatter component', () => {
-  it('renders without crashing', () => {
-    render(<JsonFormatter
+  it('renders without crashing and snapshot', () => {
+    const { asFragment } = render(<JsonFormatter
       key={1}
       queryKey={'[posts-one]'}
       jsonData={{val: 'test', arr: [1,2,3]}}
     />);
+
+    expect(asFragment()).toMatchSnapshot();
   });
 });
