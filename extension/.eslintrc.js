8a7f1b9c66c994d927766ac412a5dc9b00f0a106
John Dunn <johnwdunn20@gmail.com>
2024-02-22 00:40:42 -0800

    initial set up of eslint

--- extension/.eslintrc.js
+++ extension/.eslintrc.js
@@ -1,3 +1,7 @@
+// peer dependencies errored because of older versions: Installing eslint-plugin-react@latest, eslint-config-standard-with-typescript@latest, @typescript-eslint/eslint-plugin@^6.4.0, eslint@^8.0.1, eslint-plugin-import@^2.25.2, eslint-plugin-n@^15.0.0 || ^16.0.0 , eslint-plugin-promise@^6.0.0, typescript@*
+
+// I think they need to be updated in the package package.json file
+
 module.exports = {
     "env": {
         "browser": true,
