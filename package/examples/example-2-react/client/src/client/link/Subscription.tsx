3ada975b49e14fa76dbe240e7dff7f0080d48c9c
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-02-27 19:32:48 -0800

    Removed commented out code and refactored parts between index and subscription in the react example link folder

--- package/examples/example-2-react/client/src/client/link/Subscription.tsx
+++ package/examples/example-2-react/client/src/client/link/Subscription.tsx
@@ -1,47 +1,19 @@
 import React, { useEffect } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { formatData } from './formatData';
-import sendEvent from './sendEvent';
 
 type SubscriptionProps = {
-  contentConnectedRef: React.RefObject<boolean>;
-  addMessageToQueue: any;
+  handleMessages: any;
 };
 
-function Subscription({
-  contentConnectedRef,
-  addMessageToQueue,
-}: SubscriptionProps) {
+function Subscription({ handleMessages }: SubscriptionProps) {
   const queryClient = useQueryClient();
   const queryCache = queryClient.getQueryCache();
-  // const [contentMessageQueue, setContentMessageQueue] = useState<any[]>([]);
 
   const handleQueryCacheChange = async (event: any) => {
-    // Formats query cache events into message to be sent to the content script
     const message = formatData(event, queryClient);
-    // Query cache events not type 'update' are ignored
-    if (!message) return;
-
-    if (!contentConnectedRef.current) {
-      console.log('que');
-      console.log(contentConnectedRef.current);
-
-      // setContentMessageQueue(prevQueue => [...prevQueue, message]);
-
-      addMessageToQueue(message);
-    } else {
-      console.log('send');
-      console.log(contentConnectedRef.current);
-      sendEvent(message);
-    }
+    if (message) handleMessages(message);
   };
-
-  // useEffect(() => {
-  //   if (contentConnectedRef.current) {
-  //     contentMessageQueue.forEach(sendEvent);
-  //     setContentMessageQueue([]);
-  //   }
-  // }, [contentConnectedRef]);
 
   useEffect(() => {
     const unsubscribe = queryCache.subscribe(handleQueryCacheChange);
--- package/examples/example-2-react/client/src/client/link/index.tsx
+++ package/examples/example-2-react/client/src/client/link/index.tsx
@@ -1,34 +1,35 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Subscription from './Subscription';
 import TimeTravel from './TimeTravel';
-import sendEvent from './sendEvent';
 
 function ReactQueryRewind() {
   const [timeTravel, setTimeTravel] = useState(false);
-  // const [contentConnected, setContentConnected] = useState(false);
-  const contentConnectedRef = useRef(false); // Use useRef here
-
-  // const [contentMessageQueue, setContentMessageQueue] = useState<any[]>([]);
+  const contentConnectedRef = useRef(false);
   const contentMessageQueueRef = useRef<any[]>([]);
 
   const sendContentMessageQueue = () => {
-    console.log('SENDING MESSAGES');
-    // console.log('SEND_QUEUE:', contentMessageQueue);
     contentMessageQueueRef.current.forEach(sendEvent);
     contentMessageQueueRef.current = [];
   };
 
-  const addMessageToQueue = (message: any) => {
-    console.log(message);
-    // console.log('ADD_TO_QUEUE', contentMessageQueue);
-    contentMessageQueueRef.current.push(message); // Add the message directly to the ref
+  const handleMessages = (message: any) => {
+    if (contentConnectedRef.current) {
+      sendEvent(message);
+    } else {
+      contentMessageQueueRef.current.push(message);
+    }
   };
+
+  function sendEvent(event: any) {
+    window.postMessage(
+      { framework: 'react', type: 'event', payload: event },
+      '*'
+    );
+  }
 
   const handleContentMessages = (message: MessageEvent) => {
     if (message.data?.type === 'content-script-ready') {
-      // console.log('APP: Connected to content.ts');
-      // setContentConnected(true);
-      contentConnectedRef.current = true; // Directly update ref
+      contentConnectedRef.current = true;
       window.postMessage({ type: 'app-connected' }, '*');
       sendContentMessageQueue();
     }
@@ -39,8 +40,6 @@ function ReactQueryRewind() {
   };
 
   useEffect(() => {
-    // console.log('RERENDERING');
-
     window.addEventListener('message', handleContentMessages);
 
     return () => {
@@ -48,23 +47,12 @@ function ReactQueryRewind() {
     };
   }, []);
 
-  // useEffect(() => {
-  //   console.log(contentMessageQueue);
-  // }, [contentMessageQueue]);
-
-  // useEffect(() => {
-  //   console.log('Index: contentConnected', contentConnected);
-  // }, [contentConnected]);
-
   return (
     <div>
       {timeTravel ? (
         <TimeTravel />
       ) : (
-        <Subscription
-          contentConnectedRef={contentConnectedRef}
-          addMessageToQueue={addMessageToQueue}
-        />
+        <Subscription handleMessages={handleMessages} />
       )}
     </div>
   );
