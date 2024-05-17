e4cfcc9c18db69b6f8bc428bb4d9246e929f19f1
John Dunn <johnwdunn20@gmail.com>
2024-05-17 00:15:19 -0700

    added additional type safety to createDisplayArray

--- extension/src/functions/createDisplayArray.ts
+++ extension/src/functions/createDisplayArray.ts
@@ -21,14 +21,14 @@ const createDisplayArray = (
 
   // filter for events of selected queries
   const selectedQueryEvents = queryEvents.filter((queryEvent) =>
-    selectedQueries.includes(queryEvent.queryHash)
+    selectedQueries.includes(queryEvent?.queryHash)
   );
 
   // traverse queries and update the relevant query data for that event
   selectedQueryEvents.forEach((queryEvent) => {
     const prevDisplay = [...allDisplays[allDisplays.length - 1]];
     const newDisplay = prevDisplay.map((display) => {
-      if (display.queryKey === queryEvent.queryHash) {
+      if (display.queryKey === queryEvent?.queryHash) {
         return { ...display, queryData: queryEvent.queryData };
       }
       return display;
