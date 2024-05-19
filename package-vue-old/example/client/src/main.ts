ad0358d602dd103839e5d83688691254d3e4bf8d
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-05-18 18:50:21 -0700

    Added the vue example into the new directory and deleted the link folder

---
+++ package-vue-old/example/client/src/main.ts
@@ -0,0 +1,22 @@
+import { createApp } from 'vue';
+import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query';
+import App from './App.vue';
+import './styles.css';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 mins
+      gcTime: 10 * 60 * 1000, // 10 mins
+    },
+  },
+});
+
+// creating a new Vue app instance with app as the root component
+const app = createApp(App);
+
+// installing the vue query plugin into the app
+app.use(VueQueryPlugin, { queryClient });
+
+// mounts vue app to DOM with id app
+app.mount('#app');
